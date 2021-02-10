4bead44d879696c2b44ed9f28fd86316a093cedf
Sergey Samoylenko <s.r.samoylenko@gmail.com>
2021-02-10 16:04:00 +0300

    CashCalculator refactored. Docstrings added. Annotations added. get_today_stats() and get_week_stats() from Calculator refactored.

--- homework.py
+++ homework.py
@@ -1,62 +1,87 @@
 import datetime as dt
+from typing import Dict, Union
 
 
 class Record:
-    def __init__(self, amount, comment, date=None):
+    def __init__(self,
+                 amount: Union[int, float],
+                 comment: str,
+                 date: str = None) -> None:
         self.amount = amount
         self.comment = comment
-        if date:
-            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
-        else:
+        if date is None:
             self.date = dt.date.today()
+        else:
+            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
 
 
 class Calculator:
-    def __init__(self, limit):
+    def __init__(self, limit: float) -> None:
         self.limit = limit
         self.records = []
 
-    def add_record(self, record):
+    def add_record(self, record: Record) -> None:
+        """Add record to record list."""
         self.records.append(record)
 
-    def get_today_stats(self):
+    def get_today_stats(self) -> float:
+        """Return overall amount for current day."""
+        today = dt.date.today()
         return sum([record.amount for record in self.records
-                    if record.date == dt.date.today()])
+                    if record.date == today])
 
-    def get_week_stats(self):
+    def get_week_stats(self) -> float:
+        """Return overall amount for last week."""
+        today = dt.date.today()
+        week_ago = today - dt.timedelta(weeks=1)
         return sum([record.amount for record in self.records
-                    if dt.date.today() - dt.timedelta(days=7)
-                    <= record.date <= dt.date.today()])
+                    if week_ago < record.date <= today])
 
 
 class CaloriesCalculator(Calculator):
-    def get_calories_remained(self):
+    def get_calories_remained(self) -> str:
+        """Tell whether one could eat something more for today."""
         calories_remained = self.limit - self.get_today_stats()
         if calories_remained > 0:
             return (f"Сегодня можно съесть что-нибудь ещё, но с общей "
                     f"калорийностью не более {calories_remained} кКал")
-        else:
-            return "Хватит есть!"
+        return "Хватит есть!"
 
 
 class CashCalculator(Calculator):
+    RUB_RATE = 1.0
     USD_RATE = 74.37
     EURO_RATE = 89.59
 
-    def get_today_cash_remained(self, currency):
-        cash_remained = self.limit - self.get_today_stats()
-        cur = 'руб'
-        if currency == 'usd':
-            cash_remained /= CashCalculator.USD_RATE
-            cur = 'USD'
-        elif currency == 'eur':
-            cash_remained /= CashCalculator.EURO_RATE
-            cur = 'Euro'
+    def get_currences(self) -> Dict:
+        """Return currency names and up to date rates."""
+        return {
+            'rub': {
+                'name': 'руб',
+                'rate': self.RUB_RATE
+            },
+            'usd': {
+                'name': 'USD',
+                'rate': self.USD_RATE
+            },
+            'eur': {
+                'name': 'Euro',
+                'rate': self.EURO_RATE
+            }
+        }
 
-        if cash_remained > 0:
-            return f"На сегодня осталось {cash_remained:.2f} {cur}"
-        elif cash_remained == 0:
+    def get_today_cash_remained(self, currency: str = 'rub') -> str:
+        """Tell whether one could spend more money today."""
+        cash_remained = self.limit - self.get_today_stats()
+        currences = self.get_currences()
+
+        if cash_remained == 0:
             return "Денег нет, держись"
+        elif cash_remained > 0:
+            cash_remained /= currences[currency]['rate']
+            return (f"На сегодня осталось "
+                    f"{cash_remained:.2f} {currences[currency]['name']}")
         else:
-            debt = abs(cash_remained)
-            return f"Денег нет, держись: твой долг - {debt:.2f} {cur}"
+            debt = abs(cash_remained) / currences[currency]['rate']
+            return (f"Денег нет, держись: твой долг - "
+                    f"{debt:.2f} {currences[currency]['name']}")
